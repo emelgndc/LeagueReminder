93f0765b39a61adc5176811eb574fea3552c04d1
Andy <chena14139@gmail.com>
2020-12-15 22:41:54 +1300

    Mostly functioning GUI. Next: configure actual setting (and saving) of reminders, and implement pop-up window for reminder/invalid input alert.

--- src/main/java/com/leagueReminder/maven/classes/App.java
+++ src/main/java/com/leagueReminder/maven/classes/App.java
@@ -5,15 +5,20 @@ import com.merakianalytics.orianna.types.core.summoner.Summoner;
 
 public class App 
 {
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
         Orianna.loadConfiguration("config.json");
-        Orianna.setRiotAPIKey("API KEY HERE");
+        Orianna.setRiotAPIKey("RGAPI-28462425-e6e3-4b16-accb-4aa0def7b67e");
         
         Window window = new Window();
         //Summoner emelg = Summoner.named("emelg").get();
         //System.out.println(emelg.getLevel());
         
+    }
+    
+    public void setReminder() {
+    	
     }
 }
--- src/main/java/com/leagueReminder/maven/classes/Window.java
+++ src/main/java/com/leagueReminder/maven/classes/Window.java
@@ -1,14 +1,88 @@
 package com.leagueReminder.maven.classes;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Window {
+	private String summonerName;
+	private String reminderText;
+	private int numGames;
+	private JTextField stext;
+	private JTextField rtext;
+	private JTextField ntext;	
 	
 	public Window() {
+		//Creating the frame
 		JFrame frame = new JFrame("League Reminder App");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(800,400);
-	    //JButton button = new JButton("Press");
-	    //frame.getContentPane().add(button); // Adds Button to content pane of frame
+		frame.setSize(450,200);
+		frame.setResizable(false);
+		
+
+	    //Menu bar
+	    JMenuBar mb = new JMenuBar();
+	    JMenu file = new JMenu("File");
+	    JMenuItem rem = new JMenuItem("Active Reminders");
+	    JMenuItem exit = new JMenuItem("Exit");
+	    file.add(rem);
+	    file.add(exit);
+	    mb.add(file);
+	    
+		//TextFields
+		stext = new JTextField(15);
+		rtext = new JTextField(25);
+		ntext = new JTextField(2);
+		
+		//Labels
+		JLabel slabel = new JLabel("Summoner name:");
+		JLabel rlabel = new JLabel("Reminder text:");
+		JLabel nlabel1 = new JLabel("Repeat for");
+		JLabel nlabel2 = new JLabel("games (0 for unlimited)");
+		
+	    //Panels
+	    JPanel panel = new JPanel();
+	    JPanel summonerNamePanel = new JPanel();
+	    JPanel reminderPanel = new JPanel();
+	    JPanel numGamesPanel = new JPanel();
+	    
+	    summonerNamePanel.add(slabel);
+	    summonerNamePanel.add(stext);
+	    reminderPanel.add(rlabel);
+	    reminderPanel.add(rtext);
+	    numGamesPanel.add(nlabel1);
+	    numGamesPanel.add(ntext);
+	    numGamesPanel.add(nlabel2);
+	    
+		//Button
+	    JButton button = new JButton("Set");
+	    button.addActionListener(new ActionListener() {
+	    	@Override
+	    	public void actionPerformed(ActionEvent e) { //when 'set' is pressed
+	    		try {
+	    			numGames = Integer.parseInt(ntext.getText());
+		    		summonerName = stext.getText();
+		    		reminderText = rtext.getText();
+		    		System.out.println(numGames);
+		    		System.out.println(summonerName);
+		    		System.out.println(reminderText);		// execute setreminder
+	    		} catch (NumberFormatException x) {
+	    			System.out.println("needs to be a number"); // popup window notifying user of invalid input
+	    		}
+	    		//System.out.println(summonerName);
+	    	}
+	    });
+	    
+	    //Adding components to center panel
+	    panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+	    panel.add(summonerNamePanel);
+	    panel.add(reminderPanel);
+	    panel.add(numGamesPanel);
+	    
+	    //Adding components to frame
+	    frame.getContentPane().add(BorderLayout.PAGE_START, mb);
+	    frame.getContentPane().add(BorderLayout.CENTER, panel);
+	    frame.getContentPane().add(BorderLayout.PAGE_END, button);
 	    frame.setVisible(true);
 	}
 
